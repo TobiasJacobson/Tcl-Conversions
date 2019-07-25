ca023cdd90a1b224ed1feb5bfc84035ca779fe95
Tobias Jacobson <44820966+TobiasJacobson@users.noreply.github.com>
2019-07-25 11:18:18 -0700

    Adding group_contents

---
+++ group_contentsPy.py
@@ -0,0 +1,49 @@
+
+
+def group_autoload():
+    gFilenames = []
+    grpdir = gFilenames[groups_dir]
+    # Group Stuff
+    group_readProfiles(lsubcl3) = grpdir + 'lsubcl3'
+    group_readProfiles(rcca) = grpdir + 'rcca'
+    group_readProfiles(rima) = grpdir + 'rima'
+    group_readProfiles(svg1) = grpdir + 'svg1'
+    group_readProfiles(rca4) = grpdir + 'rca4'
+    group_readProfiles(svg2) = rpdir + 'svg2'
+    group_readProfiles(lca2_2) = grpdir  +'lca2_2'
+    group_readProfiles(rsubcl1) = grpdir + 'rsubcl1'
+    group_readProfiles(rsubcl_new) = rpdir + 'rsubcl_new'
+    group_readProfiles(om) = grpdir + 'om'
+    group_readProfiles(om_bif) = grpdir + 'om_bif'
+    group_readProfiles(rsubcl2) = grpdir + 'rsubcl2'
+    group_readProfiles(om1) = grpdir + 'om1'
+    group_readProfiles(pda) = grpdir + 'pda'
+    group_readProfiles(rsubcl3) = grpdir + 'rsubcl3'
+    group_readProfiles(om2) = grpdir + 'om2'
+    group_readProfiles(lsubcl2_1) = grpdir + 'lsubcl2_1'
+    group_readProfiles(rca1_1) = grpdir + 'rca1_1'
+    group_readProfiles(om3) = grpdir + 'om3'
+    group_readProfiles(seq_svg) = grpdir + 'seq_svg'
+    group_readProfiles(lca) = grpdir +  'lca'
+    group_readProfiles(rca1_2) = grpdir + 'rca1_2'
+    group_readProfiles(om4) = grpdir + 'om4'
+    group_readProfiles(lca1) = grpdir + 'lca1'
+    group_readProfiles(lca2) = grpdir + 'lca2'
+    group_readProfiles(lca2_prox) = rpdir + 'lca2_prox'
+    group_readProfiles(lcca) = grpdir + 'lcca'
+    group_readProfiles(lca2_2_1) = grpdir + 'lca2_2_1'
+    group_readProfiles(lima) = grpdir + 'lima'
+    group_readProfiles(diamond) = grpdir + 'diamond'
+    group_readProfiles(rsubcl2_1) = grpdir + 'rsubcl2_1'
+    group_readProfiles(rca) = grpdir + 'rca'
+    group_readProfiles(lsubcl1) = grpdir + 'lsubcl1'
+    group_readProfiles(lsubcl_new) = grpdir + 'lsubcl_new'
+    group_readProfiles(rca1) = grpdir + 'rca1'
+    group_readProfiles(lsubcl2) = grpdir + 'lsubcl2'
+    group_readProfiles(aorta) = grpdir + 'aorta'
+
+# end of group_autoload
+
+def seg3d_autoload():
+    gFilenames = []
+    grpdir = gFilenames(groups_dir)
